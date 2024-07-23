189ba9fa3a3642d1b5b0caf25f088964b5992ca5
SydsBike <sydsbike64@gmail.com>
2024-07-23 04:52:39 -0500

    add  getSign function

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function getSign() {
+
+const signs = ["Aries", 'Taurus', "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces" ];
+
+
+let randomNumber = Math.floor(Math.random()* signs.length);
+
+return signs[randomNumber]
+
+}
+
+function horoscopt(sign) {
+
+    
+
+}
